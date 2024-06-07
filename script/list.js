46c949e66579750753caf5cebea2753aebba5cb2
C0dingForFun <151622640+C0dingForFun@users.noreply.github.com>
2024-06-07 15:48:44 +0200

    first step

---
+++ script/list.js
@@ -0,0 +1,14 @@
+let input = document.querySelector('input');
+let sortBtn = document.querySelectorAll('button')[0];
+let addBtn = document.querySelectorAll('button')[1];
+
+function List(id, name, createdDate, completed){
+    this.id = id;
+    this.name = name;
+    this.createdDate = createdDate;
+    this.completed = completed;
+    
+    
+}
+
+
